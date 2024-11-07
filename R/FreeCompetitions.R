1603c6a147d856d747ef44d01507228c2601f5ce
luishenrique017 <luishenriqueblauth@gmail.com>
2024-11-07 20:52:15 -0300

    Update FreeCompetitions.R

--- R/FreeCompetitions.R
+++ R/FreeCompetitions.R
@@ -1,4 +1,7 @@
-FreeCompetitions <- function(){
+Cruyff <- FreeMatches(Comps) %>%
+filter(match_id %in% c("3888711", "3750235", "3888706", "3888716",
+"3888717", "3888718", "3888719",
+"3888720", "3888713", "3888754"))
   print("Whilst we are keen to share data and facilitate research, we also urge you to be responsible with the data. Please credit StatsBomb as your data source when using the data and visit https://statsbomb.com/media-pack/ to obtain our logos for public use.")
   Competitions.url <- "https://raw.githubusercontent.com/statsbomb/open-data/master/data/competitions.json"
   raw.competitions <- GET(url = Competitions.url)
